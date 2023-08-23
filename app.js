fd7e1e3c57fcbec66487bc433cee93231de85a7b
hamzafattni <136331328+hamzafattni@users.noreply.github.com>
2023-08-23 10:26:39 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,5 @@
+const student =['ahmed' , 'owais' , 'ahmedAli' , 'maaz' , 'ali'] 
+student.splice(3 , 1 , 'hamza')
+student.push('mustafa' , 'zain')
+student.splice(1 , 1 , 'ahsan')
+console.log(student);
